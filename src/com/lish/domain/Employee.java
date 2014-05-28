8cc244091bd5c3625521e973b79528ed77611f7a
lish <lish516@gmail.com>
2014-05-28 18:13:59 +0800

    I want to go home now.

--- src/com/lish/domain/Employee.java
+++ src/com/lish/domain/Employee.java
@@ -21,38 +21,6 @@ public class Employee {
 	@Setter @Getter
 	private Date hiredate;
 
-//	public Integer getId() {
-//		return id;
-//	}
-//
-//	public void setId(Integer id) {
-//		this.id = id;
-//	}
-//
-//	public String getName() {
-//		return name;
-//	}
-//
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//
-//	public String getEmail() {
-//		return email;
-//	}
-//
-//	public void setEmail(String email) {
-//		this.email = email;
-//	}
-//
-//	public Date getHiredate() {
-//		return hiredate;
-//	}
-//
-//	public void setHiredate(Date hiredate) {
-//		this.hiredate = hiredate;
-//	}
-
 	public Employee(){
 
 	}
