855edc9ce1a2fd32a62ee7ce54e1ee8a53e1a379
lishuang <lish516@126.com>
2014-05-29 00:01:49 +0800

    Backup the database.

---
+++ db/hibernate0529.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE  IF NOT EXISTS `hibernate` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `hibernate`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+--
+-- Host: localhost    Database: hibernate
+-- ------------------------------------------------------
+-- Server version	5.6.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `hiredate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'hanmeimei','lish516@gg.com','2014-05-28 22:33:23'),(2,'hanmeimei','lish516@gg.com','2014-05-28 22:33:35'),(3,'hanmeimei','lish516@gg.com','2014-05-28 22:33:47');
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `worker`
+--
+
+DROP TABLE IF EXISTS `worker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `worker` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `hiredate` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `worker`
+--
+
+LOCK TABLES `worker` WRITE;
+/*!40000 ALTER TABLE `worker` DISABLE KEYS */;
+INSERT INTO `worker` VALUES (1,'lish516@126.com','2014-05-28 22:30:23','workerLishuang'),(2,'lish516@126.com','2014-05-28 22:30:33','workerLishuang'),(3,'lish516@126.com','2014-05-28 22:30:42','workerLishuang'),(4,'lish516@126.com','2014-05-28 22:33:24','workerLishuang'),(5,'lish516@126.com','2014-05-28 22:33:36','workerLishuang'),(6,'lish516@126.com','2014-05-28 22:33:48','workerLishuang');
+/*!40000 ALTER TABLE `worker` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-05-29  0:01:13
--- src/com/lish/view/IndexView.java
+++ src/com/lish/view/IndexView.java
@@ -31,23 +31,34 @@ public class IndexView {
 
 	}
 
-	private static void deleteEmployee(){
+	private static void deleteEmployee() {
 		//获取一个会话。
 		Session session = MySessionFactory.getSession();
-		Transaction transaction=session.beginTransaction();
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			session.beginTransaction();
+			//获取要需改的用户
+			//load方法是可以通过主键来获取一个对象实例。和表的一条记录对应。
+			Employee employee = (Employee) session.load(Employee.class, 2);
 
-		//获取要需改的用户
-		//load方法是可以通过主键来获取一个对象实例。和表的一条记录对应。
-		Employee employee =(Employee) session.load(Employee.class,2);
+			session.delete(employee);
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			throw new RuntimeException();
 
-		session.delete(employee);
-		transaction.commit();
-		session.close();
+		} finally {
+			if (session != null && session.isOpen()) {
+
+				session.close();
+			}
+		}
+
 
 	}
-
-
-
 
 
 	//添加用户
@@ -55,10 +66,10 @@ public class IndexView {
 		System.out.println("HelloWorld");
 
 		//添加一个雇员.
-		Employee employee=new Employee("hanmeimei","lish516@gg.com",new Date());
+		Employee employee = new Employee("hanmeimei", "lish516@gg.com", new Date());
 		//获取一个会话。
 		Session session = MySessionFactory.getSession();
-		Transaction transaction=session.beginTransaction();
+		Transaction transaction = session.beginTransaction();
 		//Insert...
 		//保存。=>insert into ... 被hibernate 封装起来了。
 
@@ -69,16 +80,15 @@ public class IndexView {
 	}
 
 
-
 	//修改用户
-	private static void editEmployee(){
+	private static void editEmployee() {
 		//获取一个会话。
 		Session session = MySessionFactory.getSession();
-		Transaction transaction=session.beginTransaction();
+		Transaction transaction = session.beginTransaction();
 
 		//获取要需改的用户
 		//load方法是可以通过主键来获取一个对象实例。和表的一条记录对应。
-		Employee employee =(Employee) session.load(Employee.class,1);
+		Employee employee = (Employee) session.load(Employee.class, 1);
 
 		employee.setName("韩顺平");
 		employee.setEmail("hanshunping@126.com");
@@ -93,10 +103,10 @@ public class IndexView {
 		System.out.println("Adding worker");
 
 		//添加一个工人.
-		Worker worker=new Worker("workerLishuang","lish516@126.com",new Date());
+		Worker worker = new Worker("workerLishuang", "lish516@126.com", new Date());
 		//获取一个会话。
 		Session session = MySessionFactory.getSession();
-		Transaction transaction=session.beginTransaction();
+		Transaction transaction = session.beginTransaction();
 		//Insert...
 		//保存。=>insert into ... 被hibernate 封装起来了。
 
