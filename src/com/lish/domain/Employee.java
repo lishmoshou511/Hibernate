a5bdd6dfff20b7cbff2615d7af7d5a93e2aafb98
lish <lish516@gmail.com>
2014-05-28 16:10:38 +0800

    Finish the first runnable project of hibernate.

--- src/com/lish/domain/Employee.java
+++ src/com/lish/domain/Employee.java
@@ -10,13 +10,56 @@ import java.util.Date;
  * Created by InnoXYZ on 14-5-28.
  */
 public class Employee {
-	@Getter @Setter
-	private int id;
-	@Getter @Setter
+
+
+	@Setter @Getter
+	private Integer id;
+	@Setter @Getter
 	private String name;
-	@Getter @Setter
+	@Setter @Getter
 	private String email;
-	@Getter @Setter
+	@Setter @Getter
 	private Date hiredate;
 
+//	public Integer getId() {
+//		return id;
+//	}
+//
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//	public Date getHiredate() {
+//		return hiredate;
+//	}
+//
+//	public void setHiredate(Date hiredate) {
+//		this.hiredate = hiredate;
+//	}
+
+	public Employee(){
+
+	}
+
+	public Employee(String name, String email, Date hiredate) {
+		this.name = name;
+		this.email = email;
+		this.hiredate = hiredate;
+	}
 }
---
+++ src/com/lish/view/IndexView.java
@@ -0,0 +1,51 @@
+package com.lish.view;
+
+
+import com.lish.domain.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+import java.util.Date;
+
+/**
+ * Created by InnoXYZ on 14-5-28.
+ */
+public class IndexView {
+
+
+	public static void main(final String[] args) throws Exception {
+		System.out.println("HelloWorld");
+
+		//添加一个雇员.
+		Employee employee=new Employee("liuxiang","lish516@qq.com",new Date());
+
+
+
+
+		//1.创建Configuration,该对象用于读取hiberante.cfg.xml并完成初始化。
+		Configuration configuration=new Configuration().configure();
+		//2.创建sessionFactory，这是一个重量级的类。应该做出单例。
+		SessionFactory sessionFactory=configuration.buildSessionFactory();
+		//3.创建session,相当于jdbc Connection
+		Session session=sessionFactory.openSession();
+		//4.对hibernate而言，要求程序员，在进行 增加 删除 修改的时候必须要使用事务提交。
+		Transaction transaction=session.beginTransaction();
+
+
+
+
+		//Insert...
+		//保存。=>insert into ... 被hibernate 封装起来了。
+		session.save(employee);
+
+		transaction.commit();
+		session.close();
+
+
+
+
+	}
+}
